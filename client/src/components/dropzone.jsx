442c3349fe61dc9bbc64b6c862dcb5ef94e46ff9
Kyle Shifflett <kshifflett707@gmail.com>
2018-10-29 19:57:50 -0700

    Changed CLIENT-ID key for imgur API

--- client/src/components/dropzone.jsx
+++ client/src/components/dropzone.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Dropzone from 'react-dropzone';
-import axios from 'axios';
-import { BounceLoader } from 'react-spinners';
+import React from "react";
+import ReactDOM from "react-dom";
+import Dropzone from "react-dropzone";
+import axios from "axios";
+import { BounceLoader } from "react-spinners";
 
-import EXIF from 'exif-js';
-import Promise from 'bluebird';
-import $ from 'jquery';
+import EXIF from "exif-js";
+import Promise from "bluebird";
+import $ from "jquery";
 
 /*
 This component helps to upload images by providing a 
@@ -19,13 +19,13 @@ but in this case we only append the 0th element of the array (the first item).
 
 export default class Accept extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
       accepted: [],
       rejected: [],
       uploaded: false,
       loading: false
-    }
+    };
     this.changeImg = this.changeImg.bind(this);
   }
 
@@ -34,31 +34,40 @@ export default class Accept extends React.Component {
     let dataArr = [];
     const that = this;
 
-    let toDecimal = function (meta) {
-      return meta[0].numerator + meta[1].numerator /
-        (60 * meta[1].denominator) + meta[2].numerator / (3600 * meta[2].denominator);
+    let toDecimal = function(meta) {
+      return (
+        meta[0].numerator +
+        meta[1].numerator / (60 * meta[1].denominator) +
+        meta[2].numerator / (3600 * meta[2].denominator)
+      );
     };
-  
+
     for (let i = 0; i < img.length; i++) {
       dataArr[i] = new FormData();
-      dataArr[i].append('image', img[i]);
+      dataArr[i].append("image", img[i]);
       //Uses EXIF to parse GPS coordinates from .jpeg images
       EXIF.getData(img[i], function() {
-          let metaTags = EXIF.getAllTags(this);
-  
-          let lat, lng;
-  
-          lat = metaTags.GPSLatitudeRef === 'N' ? toDecimal(metaTags.GPSLatitude) : toDecimal(metaTags.GPSLatitude) * -1;
-          lng = metaTags.GPSLongitudeRef === 'E' ? toDecimal(metaTags.GPSLongitude) : toDecimal(metaTags.GPSLongitude) * -1;
-          let latLng = { lat: lat, lng: lng };
-          that.props.setLocation(latLng);     
+        let metaTags = EXIF.getAllTags(this);
+
+        let lat, lng;
+
+        lat =
+          metaTags.GPSLatitudeRef === "N"
+            ? toDecimal(metaTags.GPSLatitude)
+            : toDecimal(metaTags.GPSLatitude) * -1;
+        lng =
+          metaTags.GPSLongitudeRef === "E"
+            ? toDecimal(metaTags.GPSLongitude)
+            : toDecimal(metaTags.GPSLongitude) * -1;
+        let latLng = { lat: lat, lng: lng };
+        that.props.setLocation(latLng);
       });
     }
 
     this.setState({
       loading: true,
       uploaded: false
-    })
+    });
 
     // dataArr.forEach((form) => {
     //   axios({
@@ -76,36 +85,55 @@ export default class Accept extends React.Component {
     //   })
     // })
 
-    Promise.map(dataArr, (form) => {
+    Promise.map(dataArr, form => {
       return axios({
-        method: 'post',
-        url: 'https://api.imgur.com/3/image',
-        headers: {Authorization: "Client-ID 3f9b22888755abe"},
+        method: "post",
+        url: "https://api.imgur.com/3/image",
+        headers: { Authorization: "Client-ID b13c02768a20879" },
         data: form
       })
-      .then((response) => {
-        console.log(response);
-        that.props.getLink(response.data.data.link);
-      }).catch((err) => {
-        console.log('this is your err message');
-      })
+        .then(response => {
+          console.log(response);
+          that.props.getLink(response.data.data.link);
+        })
+        .catch(err => {
+          console.log("this is your err message");
+        });
     }).then(() => {
       that.setState({
         loading: false,
         uploaded: true
-      })
-    })
+      });
+    });
   }
 
   changeImg() {
     if (this.state.loading) {
-      return <BounceLoader color={'#87ceff'} loading={this.state.loading} size={100}/>
+      return (
+        <BounceLoader
+          color={"#87ceff"}
+          loading={this.state.loading}
+          size={100}
+        />
+      );
     } else if (this.state.uploaded) {
-      return <img width="100px" height="100px" src="http://pluspng.com/img-png/success-png-success-icon-image-23194-400.png" />
+      return (
+        <img
+          width="100px"
+          height="100px"
+          src="http://pluspng.com/img-png/success-png-success-icon-image-23194-400.png"
+        />
+      );
     } else {
-      return <img width="100px" height="100px" src="http://www.arcdocendi.com/Forms/images/upload.png" />
+      return (
+        <img
+          width="100px"
+          height="100px"
+          src="https://png.icons8.com/metro/100/000000/upload.png"
+        />
+      );
     }
-    console.log('accepted array', this.state.uploaded)
+    console.log("accepted array", this.state.uploaded);
   }
 
   render() {
@@ -114,14 +142,28 @@ export default class Accept extends React.Component {
         <div className="dropzone">
           <Dropzone
             accept="image/jpeg, image/png"
-            onDrop={(this.onDrop.bind(this))}
-            style={{width: `100%`, border:`2px dashed grey`, height: `400px`}}
+            onDrop={this.onDrop.bind(this)}
+            style={{
+              width: `100%`,
+              border: `2px dashed grey`,
+              height: `400px`
+            }}
           >
-            <div style={{width: `80%`, margin:`auto`, paddingTop: `50px`, paddingBottom: `50px`, textAlign: `center`}}>
-              <p>Try dropping a file here, or click to select files to upload.</p>
+            <div
+              style={{
+                width: `80%`,
+                margin: `auto`,
+                paddingTop: `50px`,
+                paddingBottom: `50px`,
+                textAlign: `center`
+              }}
+            >
+              <p>
+                Try dropping a file here, or click to select files to upload.
+              </p>
               <p>Only *.jpeg and *.png images will be accepted</p>
             </div>
-            <div style={{width:`100px`, margin: `auto`}}>
+            <div style={{ width: `100px`, margin: `auto` }}>
               {this.changeImg()}
             </div>
           </Dropzone>
@@ -129,15 +171,19 @@ export default class Accept extends React.Component {
         <aside>
           {/* <h4>Accepted files</h4> */}
           <ul>
-            {
-              this.state.accepted.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
-            }
+            {this.state.accepted.map(f => (
+              <li key={f.name}>
+                {f.name} - {f.size} bytes
+              </li>
+            ))}
           </ul>
           {/* <h4>Rejected files</h4> */}
           <ul>
-            {
-              this.state.rejected.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
-            }
+            {this.state.rejected.map(f => (
+              <li key={f.name}>
+                {f.name} - {f.size} bytes
+              </li>
+            ))}
           </ul>
         </aside>
       </section>
--- database/mongoose.js
+++ database/mongoose.js
@@ -1,13 +1,14 @@
+const mongoose = require("mongoose");
+mongoose.promise = require("bluebird");
 
-const mongoose = require('mongoose');
-mongoose.promise = require('bluebird');
-
-const DB_URL = process.env.DB_URL || 'mongodb://localhost/PicWorthy';
+const DB_URL =
+  process.env.DB_URL ||
+  "mongodb://kyle:kyle123@ds145563.mlab.com:45563/pic-worthy";
 mongoose.connect(DB_URL);
 
 const db = mongoose.connection;
 
-db.on('error', () => console.log('error connecting to database!'));
-db.once('open', () => console.log('connection successful!'));
+db.on("error", () => console.log("error connecting to database!"));
+db.once("open", () => console.log("connection successful!"));
 
 module.exports = db;
